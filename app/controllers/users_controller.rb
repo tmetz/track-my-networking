a71a1175d223265aa2298e06853856ec572290b0
Tammy Metz <tammy@tammymetz.com>
2019-01-04 20:30:30 -0500

    create signup view and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+
+    get '/signup' do
+      if logged_in?
+        redirect '/persons'
+      else
+        erb :'users/signup'
+      end
+    end
+
+    post '/signup' do
+      if !params[:email].empty? && !params[:password].empty?
+        @user = User.create(email: params[:email], password: params[:password])
+        session[:user_email] = @user.email
+        redirect "/persons"
+      else
+        redirect "/signup"
+      end
+    end
+
+    get "/login" do
+        if logged_in?
+            redirect "/persons"
+        else
+            erb :"/users/login"
+        end
+    end
+
+    get '/logout' do
+      if logged_in?
+        session.clear
+        redirect '/login'
+      else
+        redirect '/failure'
+      end
+    end
+
+    get "/users/:slug" do
+        @user = User.find_by_slug(params[:slug])
+        @persons = Person.where(["user_id = ?", @user.id])
+        erb :'/persons/index'
+    end
+
+    post "/login" do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/persons"
+        else
+            redirect "/failure"
+        end
+    end
+
+    get "/failure" do
+        erb :failure
+    end
+
+
+
+end
