567ca002dba5cbcfbe58d152b722a9bf9480f706
Tammy Metz <tammy@tammymetz.com>
2019-01-06 17:37:13 -0500

    fix how a user's list of people contacted is generated

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -4,7 +4,7 @@ class PersonsController < ApplicationController
         if logged_in?
             @user = User.find_by_slug(params[:slug])
             if current_user == @user # don't want a user to be able to see another user's activity
-              @persons = Person.where(["user_id = ?", @user.id])
+              @persons = @user.persons
               erb :'/persons/index'
             else
               redirect '/login'
