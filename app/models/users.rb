469c675844ca580977f77d1bc36298bb1572a730
Tammy Metz <tammy@tammymetz.com>
2019-01-06 13:23:54 -0500

    add slug functionality to users

--- app/models/users.rb
+++ app/models/users.rb
@@ -2,4 +2,19 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :interactions
     has_many :persons, through: :interactions
+
+    def slug
+        new_name = self.name.downcase
+        new_name.gsub!(/[!@%&"]/,'')
+        new_name.gsub!(/\s/,'-')
+    end
+    
+    def self.find_by_slug(slug)
+        self.all.each do |user|
+            if user.slug == slug
+                return user
+            end
+        end
+        return nil
+    end
 end
