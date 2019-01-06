3ecc0510de49c5c05b82bafb10976be2f74368b6
Tammy Metz <tammy@tammymetz.com>
2019-01-06 14:06:21 -0500

    re-do database migrations

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20190105014835) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
+    t.string   "name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
