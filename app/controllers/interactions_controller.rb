a2b82b87835460c7bd23cde0dbda6b66b769fe2e
Tammy Metz <tammy@tammymetz.com>
2019-01-12 15:01:17 -0500

    add validation to interaction show controller to verify that the user is allowed to view the interaction

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -12,7 +12,11 @@ class InteractionsController < ApplicationController
     get "/interactions/:id" do
         if logged_in?
             @interaction = Interaction.find_by_id(params[:id])
-            erb :'/interactions/show'
+            if @interaction.user_id == current_user.id
+                erb :'/interactions/show'
+            else
+                redirect '/failure'
+            end
         else
             redirect '/login'
         end
