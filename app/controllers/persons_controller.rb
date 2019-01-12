9e0a9c6cf2f8147974c331d7e209135c0b9ff47b
Tammy Metz <tammy@tammymetz.com>
2019-01-12 18:43:42 -0500

    add edit person view and controllers

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -26,4 +26,47 @@ class PersonsController < ApplicationController
         end
     end
 
+    get '/persons/:id/edit' do
+        if logged_in?
+            @user = current_user
+            @person = Person.find_by_id(params[:id])
+            if @user.persons.include?(@person)
+                erb :"/persons/edit"
+            else
+                flash[:message] = "This is not one of your connections, sorry."
+                redirect '/failure'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/persons/:id' do
+        @person = Person.find_by_id(params[:id])
+        if @person.name != params[:person][:name]
+            @person.name = params[:person][:name]
+        end
+        if @person.company_name != params[:person][:company_name]
+            @person.company_name = params[:person][:company_name]
+        end
+        if @person.subjects != params[:person][:subjects]
+            @person.subjects = params[:person][:subjects]
+        end
+        if @person.meeting_place != params[:person][:meeting_place]
+            @person.meeting_place = params[:person][:meeting_place]
+        end
+        if @person.linkedin != params[:person][:linkedin]
+            @person.linkedin = params[:person][:linkedin]
+        end
+        if @person.email != params[:person][:email]
+            @person.email = params[:person][:email]
+        end
+        if @person.phone != params[:person][:phone]
+            @person.phone = params[:person][:phone]
+        end
+        @person.save
+        flash[:message] = "Successfully updated professional contact."
+        redirect to ("/persons/#{@person.id}")
+    end
+
 end
