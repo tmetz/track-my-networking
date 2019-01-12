3db7213b63ff8d3eb6eed628a9f4ad4125c316e1
Tammy Metz <tammy@tammymetz.com>
2019-01-12 14:57:30 -0500

    create interaction show page

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -9,4 +9,13 @@ class InteractionsController < ApplicationController
         end
     end
 
+    get "/interactions/:id" do
+        if logged_in?
+            @interaction = Interaction.find_by_id(params[:id])
+            erb :'/interactions/show'
+        else
+            redirect '/login'
+        end
+    end
+
 end
