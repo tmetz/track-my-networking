baf03e4d3800eab326c06ea4a0eefbe60b272ec3
Tammy Metz <tammy@tammymetz.com>
2019-01-14 12:59:23 -0500

    comment out slug functionality from User model

--- app/models/users.rb
+++ app/models/users.rb
@@ -3,18 +3,18 @@ class User < ActiveRecord::Base
     has_many :interactions
     has_many :persons, through: :interactions
 
-    def slug
-        new_name = self.name.downcase
-        new_name.gsub!(/[!@%&"]/,'')
-        new_name.gsub!(/\s/,'-')
-    end
+    # def slug
+    #     new_name = self.name.downcase
+    #     new_name.gsub!(/[!@%&"]/,'')
+    #     new_name.gsub!(/\s/,'-')
+    # end
     
-    def self.find_by_slug(slug)
-        self.all.each do |user|
-            if user.slug == slug
-                return user
-            end
-        end
-        return nil
-    end
+    # def self.find_by_slug(slug)
+    #     self.all.each do |user|
+    #         if user.slug == slug
+    #             return user
+    #         end
+    #     end
+    #     return nil
+    # end
 end
