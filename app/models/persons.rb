be77efb5231bbc76bdd1baad2681877d605c83c3
Tammy Metz <tammy@tammymetz.com>
2019-01-21 10:20:46 -0500

    change models from a bidirectional has_many through relationship to one directional

--- app/models/persons.rb
+++ app/models/persons.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
     self.table_name = "persons" # otherwise ActiveRecord will look for "people" table
-    has_many :interactions
-    has_many :users, through: :interactions
+    belongs_to :interactions
+    # has_many :interactions
+    # has_many :users, through: :interactions
 end
