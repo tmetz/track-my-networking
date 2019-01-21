18935ea02c501eb1505fa9bad9ef5bb8f602f00b
Tammy Metz <tammy@tammymetz.com>
2019-01-21 10:27:17 -0500

    tweak join table relationships

--- app/models/interactions.rb
+++ app/models/interactions.rb
@@ -1,6 +1,5 @@
 class Interaction < ActiveRecord::Base
-    #belongs_to :person
-    has_one :person
+    belongs_to :person
     belongs_to :user
 
     def self.create_formatted_date(year, month, day)
--- app/models/persons.rb
+++ app/models/persons.rb
@@ -1,6 +1,7 @@
 class Person < ActiveRecord::Base
     self.table_name = "persons" # otherwise ActiveRecord will look for "people" table
-    belongs_to :interactions
+    has_many :interactions
+    has_one :user, through: :interactions
     # has_many :interactions
     # has_many :users, through: :interactions
 end
