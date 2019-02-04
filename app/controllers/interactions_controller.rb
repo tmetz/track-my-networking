4be327388cd49d1c8c919f54c8e880835f1b5d09
Tammy Metz <tammy@tammymetz.com>
2019-02-04 18:53:21 -0500

    Refactor user authentication when user attemps to access a different user's interaction

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -58,34 +58,45 @@ class InteractionsController < ApplicationController
     get '/interactions/:id/edit' do
         if logged_in?
             @user = current_user
-            @interaction = Interaction.find_by_id(params[:id])
-            @people = @user.persons.uniq
-            erb :'/interactions/edit'
+            if (Interaction.find(params[:id]).user == @user)
+                @interaction = Interaction.find_by_id(params[:id])
+                @people = @user.persons.uniq
+                erb :'/interactions/edit'
+            else
+                flash[:message] = "You do not have access to this interaction."
+                redirect '/failure'
+            end
         else
+            flash[:message] = "You must be logged in to edit this interaction."
             redirect '/login'
         end
     end
 
     patch '/interactions/:id' do
         @interaction = Interaction.find_by_id(params[:id])
-        int_date = Interaction.create_formatted_date(params[:interaction][:year], params[:interaction][:month], params[:interaction][:day])
-        if @interaction.date != int_date
-            @interaction.date = int_date
-        end
-        if params[:interaction][:person_id] == "0"
-            @interaction.person = Person.create(params[:person])
-        elsif !params[:interaction][:person_id].empty? && params[:interaction][:person_id] != nil
-            @interaction.person = Person.find_by_id(params[:interaction][:person_id])
+        if (@interaction.user == current_user)  
+            int_date = Interaction.create_formatted_date(params[:interaction][:year], params[:interaction][:month], params[:interaction][:day])
+            if @interaction.date != int_date
+                @interaction.date = int_date
+            end
+            if params[:interaction][:person_id] == "0"
+                @interaction.person = Person.create(params[:person])
+            elsif !params[:interaction][:person_id].empty? && params[:interaction][:person_id] != nil
+                @interaction.person = Person.find_by_id(params[:interaction][:person_id])
+            else
+                flash[:message] = "You must associate your interaction with a professional contact."
+                redirect '/failure'
+            end
+            if @interaction.notes != params[:interaction][:notes]
+                @interaction.notes = params[:interaction][:notes]
+            end
+            @interaction.save
+            flash[:message] = "Successfully updated interaction."
+            redirect to ("/interactions/#{@interaction.id}")
         else
-            flash[:message] = "You must associate your interaction with a professional contact."
+            flash[:message] = "You may not edit somebody else's interaction."
             redirect '/failure'
         end
-        if @interaction.notes != params[:interaction][:notes]
-            @interaction.notes = params[:interaction][:notes]
-        end
-        @interaction.save
-        flash[:message] = "Successfully updated interaction."
-        redirect to ("/interactions/#{@interaction.id}")
     end
 
     delete '/interactions/:id/delete' do
